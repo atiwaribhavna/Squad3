bd334bf40ee909d8c8bc0ca16c76dd5bebc37c56
naina <nanu02d8@gmail.com>
2022-07-28 16:09:40 +0530

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+	let emp = 909;
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path={`/portfolios/${emp}`} element={<Navbar />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
